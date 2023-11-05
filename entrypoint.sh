7fe9e3027b329bb8882a2a65bd83d040d192ebbd
Michael Smith <2015782+mikesmitty@users.noreply.github.com>
2023-11-05 18:24:13 -0500

    fix: remove rtl-sdr server (#12)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-rtl_tcp &
 rtlamr | rtlamr-collect &
 
 while true; do
-  pgrep -x rtlamr >/dev/null || killall rtlamr-collect rtl_tcp
+  pgrep -x rtlamr >/dev/null || killall rtlamr-collect
   pgrep -x rtlamr-collect >/dev/null || exit 1
   sleep 60
 done
